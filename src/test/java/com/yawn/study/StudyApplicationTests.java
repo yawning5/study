f666df223be2e886d20af6f476167cdc6b6ee57e
yawning5 <yawning055@gmail.com>
2024-09-04 23:15:34 +0900

    reset??

--- src/test/java/com/yawn/study/StudyApplicationTests.java
+++ src/test/java/com/yawn/study/StudyApplicationTests.java
@@ -3,6 +3,8 @@ package com.yawn.study;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+// git reset 연습 이게 반영되면 리셋 성공
 @SpringBootTest
 class StudyApplicationTests {
 
