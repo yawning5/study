5b367408926287c75107062b59f1ddb71e38b84e
yawning5 <yawning055@gmail.com>
2024-09-20 21:31:25 +0900

    서버에 Docker 컨테이너가 완전종료가 되지않아 생기는 오류 수정중
    - 이전 컨테이너 삭제명령 추가
    - 워크 플로우 실행 실패로 다시 푸시
    - 주석 설정 이상으로 플로우 실행실패로 재수정
    - stop -> kill  로 변경
    - actions 버전 변경
    - docker 컨테이너 버전관리를 위한 이전 컨테이너 삭제 명령 지움
    - actions/setup-java@v3 에서는 distribution 옵션이 필수인가봄 check-latest 도 추가
    - docker 를 이름/별명:태그 가 아니라 이름으로 관리하기로 하고 버전 관리를 위해서 이전 버전을 삭제하지 않기로함 또 버전 이름은 날짜를 넣기로 했음
    - docker 실행이 안 되는데 workflow 가 정상이라고 떠서 || true 삭제
    - || true 다시 추가 빌드와 푸쉬 할때에도 태그를 바꿔줘야 했었는데 손대지 않아 도커가 실행 안 된걸로 파악후 수정
    - 푸시 와 빌드시 VERSION 값도 날짜로 밪춰서 수정
    - 시간 기준으로 계속 VERSION 을 넣으면 차이나서 안 되기때문에 한 번 고정 시켜준다
    - 시간이 맞나 확인하는 echo 설정
    - 재수정,,
    - git 환경변수 version 을 ssh 콘솔에서 유지
    - 마지막 테스트
    - VERSION 변수에 따라 Docker 이미지를 멈추면 제대로 안 멈추게 되고 어차피 허브에는 도커 이미지가 있으니 ec2 에서는 깔끔하게 유지하기로 결정
    - 문자열 안 닫아서 생긴 오류 수정
    - 구동 확인했고 이미 실행중인 컨테이너 있을때 작동하는지 test

--- src/main/java/com/yawn/study/StudyApplication.java
+++ src/main/java/com/yawn/study/StudyApplication.java
@@ -2,7 +2,7 @@ package com.yawn.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// test
 @SpringBootApplication
 public class StudyApplication {
 
