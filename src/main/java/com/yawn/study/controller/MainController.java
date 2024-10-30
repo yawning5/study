baa5633e37308a3efe34af4c7d04d7826f8acb3c
yawning5 <yawning055@gmail.com>
2024-10-30 17:23:03 +0900

    feat: 강의 9번째 시간
    학습 내용
    SecurityContextHolder 에 저장되어있는 Authentication 객체에서 정보를 빼오는 과정
    
    Authentication 객체를 가져오려면 SecurityContextHolder 를 불러와서 .getContext() 를 호출하면, 해당 스레드의 Security Context 를 가져와서 인증된 사용자 정보 Authentication 에 접근할 수 있게 된다.
    
    해당 authentication 에서 .getName(), .getAuthorities() 등을 통해 인증된 사용자의 정보를 가져올 수 있게된다.

--- src/main/java/com/yawn/study/controller/MainController.java
+++ src/main/java/com/yawn/study/controller/MainController.java
@@ -1,14 +1,40 @@
 package com.yawn.study.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 public class MainController {
 
+    /**
+     * Model 클래스는
+     * @param model
+     * @return
+     */
     @GetMapping("/")
-    public String mainP() {
+    public String mainP(Model model) {
 
+        /*
+        사용자가 인증 한 후에는 인증 된 UserDetails 가 Authentication 객체로
+        SecurityContextHolder 에 저장되게 된다. 이렇게 저장된 객체는 스레드내에서 전역적 접근가능
+         */
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String id = authentication.getName();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+        String role = auth.getAuthority();
+
+        model.addAttribute("id", id);
+        model.addAttribute("role", role);
 
         return "main";
     }
