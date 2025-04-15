6d9d16c90f9fd674439dca5f69a8257d2def2775
yawning5 <yawning055@gmail.com>
2025-04-15 19:46:05 +0900

    feat: 강의 12번째 시간
    Inmemroy 에 사용자 정보를 넣어서 로그인
    
    UserDetailsService 에 UserDetails 로 이용자를 만든 뒤 InMemoryUserDetailsManager 에 등록시켜주면 된다.

--- src/main/java/com/yawn/study/config/SecurityConfig.java
+++ src/main/java/com/yawn/study/config/SecurityConfig.java
@@ -5,7 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -62,5 +66,23 @@ public class SecurityConfig {
 
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user1 = User.builder()
+                .username("user1")
+                .password(bCryptPasswordEncoder().encode("1234"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user2 = User.builder()
+                .username("user2")
+                .password(bCryptPasswordEncoder().encode("1234"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 }
