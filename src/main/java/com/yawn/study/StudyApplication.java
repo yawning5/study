c318153889d8a6fe16b66c03dcd5d92e9ec1a88c
yawning5 <yawning055@gmail.com>
2025-04-21 14:20:32 +0900

    feat: local sql 과 연동완료
    공통, 로컬 yml 설정 및 구동완료
    로컬 sql 연동확인 완료
    
    aws rdbms 와 연동하기 위한 prod.yml 설정 완료 workflow 를 이용해서 환경변수 설정 완료

--- src/main/java/com/yawn/study/StudyApplication.java
+++ src/main/java/com/yawn/study/StudyApplication.java
@@ -1,13 +1,29 @@
 package com.yawn.study;
 
+import com.yawn.study.memeber.entity.Member;
+import com.yawn.study.memeber.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class StudyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudyApplication.class, args);
 	}
 
+	public final MemberRepository memberRepository;
+
+	@Bean
+	CommandLineRunner init(MemberRepository memberRepository) {
+		return args -> {
+			Member member = new Member();
+			member.setName("test-user");
+			memberRepository.save(member);
+		};
+	}
 }
