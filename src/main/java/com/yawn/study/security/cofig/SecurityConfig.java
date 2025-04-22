07c532bd09a336625509d54b29ef3229424eb71b
yawning5 <yawning055@gmail.com>
2025-04-22 15:37:14 +0900

    feat: Entity 와 Repo 설정 및 security 설정 준비

--- src/main/java/com/yawn/study/security/cofig/SecurityConfig.java
+++ src/main/java/com/yawn/study/security/cofig/SecurityConfig.java
@@ -1,7 +1,22 @@
 package com.yawn.study.security.cofig;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder () {
+
+        return new BCryptPasswordEncoder();
+    }
+
+    public SecurityFilterChain filterChain() {
+
+    }
 }
